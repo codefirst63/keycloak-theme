08fd63314a0c77e0d333dca72abee5562d0503b6
Camille Petitalot <camillepetitalot@MacBook-Pro.local>
2023-07-05 12:40:17 +0200

    update keycloaksify

---
+++ src/keycloak-theme/login/pages/Info.stories.tsx
@@ -0,0 +1,14 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { createPageStory } from "../createPageStory";
+
+const { PageStory } = createPageStory({
+    pageId: "info.ftl"
+});
+
+export default {
+    title: "login/Info",
+    component: PageStory,
+} as ComponentMeta<typeof PageStory>;
+
+export const Default: ComponentStory<typeof PageStory> = () => <PageStory />;
+
